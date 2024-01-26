ab3db0faedf4f0b12319c258280cf2a7ae6487cb
sawfee <57741982+sawfee@users.noreply.github.com>
2024-01-26 10:41:01 +0700

    dd
    ddd

--- nuxt.config.js
+++ nuxt.config.js
@@ -19,7 +19,7 @@ export default {
    ** Headers of the page
    */
   head: {
-    title: "Nuxt Black Dashboard",
+    title: "Sophee Dashboard",
     meta: [
       { charset: "utf-8" },
       { name: "viewport", content: "width=device-width, initial-scale=1" },
